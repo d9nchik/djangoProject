b6035775f869952602742e2c5e62fa7d7018a35e
d9nich <dan.galaiko@gmail.com>
2020-08-18 21:10:42 +0300

    Simple application in Django.

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from movies.views import MovieView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', MovieView.as_view()),
 ]
